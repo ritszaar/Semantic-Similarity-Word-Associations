5d98e5cc6ab0ab645795446d3f25cb587a1cbcf5
Ritwik <mallikritwik2014@gmail.com>
2023-04-01 22:25:17 +0530

    Complete base model

--- main.py
+++ main.py
@@ -1,16 +1,113 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import pickle
+import cv2
+import PIL
 import queue
+import random
+import bisect
+import pickle
+import datasets
+import psycopg2
 import numpy as np
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
+topIWK = 4
+topWWK = 4
+
+def connect():
+    print("Connecting to PostgreSQL database...")
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="word_associations",
+            user="miranda",
+            password="1234")
+        return conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
 def db_format(label):
     return label.replace('_', ' ').upper()
 
+def resize_image_for_resnet(img):
+    rgb_img = img.convert('RGB')
+    img_array = np.array(rgb_img)
+    resized_img_array = cv2.resize(img_array, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+    return PIL.Image.fromarray(resized_img_array)
+
+def get_predictions(img):
+    # Load the pre-trained ResNet50 model
+    model = tf.keras.applications.resnet50.ResNet50(weights='imagenet')
+
+    # Convert the image to a numpy array
+    x = tf.keras.preprocessing.image.img_to_array(img)
+
+    # Reshape the array to match the input shape of the model
+    x = np.expand_dims(x, axis=0)
+    x = tf.keras.applications.resnet50.preprocess_input(x)
+
+    # Use the model to classify the image
+    preds = model.predict(x)
+
+    decoded_preds = tf.keras.applications.resnet50.decode_predictions(preds, top=10)[0]
+    pred_associated_words = []
+    for decoded_pred in decoded_preds:
+        pred_associated_words.append((db_format(decoded_pred[1]), decoded_pred[2]))
+    return pred_associated_words
+
+class CIFARData:
+    def __init__(self, which="train"):
+        # Load the CIFAR 100 dataset
+        print("\nLoading the CIFAR 100 {} dataset...".format(which))
+        dataset = datasets.load_dataset('cifar100')
+        print("\nSuccessfully loaded the CIFAR 100 {} dataset.\n".format(which))
+
+        # Get the fine labels and store the mappings from fine_label to id and vice versa
+        fine_labels = dataset[which].features["fine_label"].names
+        fine_label2id, id2fine_label = dict(), dict()
+
+        for i, fine_label in enumerate(fine_labels):
+            fine_label2id[fine_label] = i
+            id2fine_label[i] = fine_label
+
+        # Get the coarse labels and store the mappings from coarse_label to id and vice versa
+        coarse_labels = dataset[which].features["coarse_label"].names
+        coarse_label2id, id2coarse_label = dict(), dict()
+
+        for i, coarse_label in enumerate(coarse_labels):
+            coarse_label2id[coarse_label] = i
+            id2coarse_label[i] = coarse_label
+
+        self.dataset = dataset[which]
+        self.fine_labels  = fine_labels
+        self.coarse_labels = coarse_labels
+        self.fine_label2id = fine_label2id
+        self.id2fine_label = id2fine_label
+        self.coarse_label2id = coarse_label2id
+        self.id2coarse_label = id2coarse_label
+    
+    def get_fine_label_by_index(self, index):
+        return self.id2fine_label[self.dataset[index]["fine_label"]]
+    
+def get_new_words(all_words, base_new_words):
+    all_new_words = set()
+    for base_new_word in base_new_words:
+        all_new_words.add(base_new_word)
+        cue = base_new_word.split(" ")[-1].replace("'", "''")
+        cursor = conn.cursor()
+        cursor.execute("SELECT target, strength FROM usf_word_associations WHERE cue='{}'".format(cue))
+        rows = cursor.fetchall()
+        derived_words_count = min(len(rows) if rows is not None else 0, topWWK)
+        for i in range(derived_words_count):
+            if bisect.bisect_left(all_words, rows[i][0]) == len(all_words):
+                all_new_words.add(rows[i][0])
+        cursor.close()
+    all_new_words = sorted(all_new_words)
+    return all_new_words
 class WordAssociationsNetwork:
     def __init__(self):
         with open("words.pickle", "rb") as f:
@@ -30,13 +127,13 @@ class WordAssociationsNetwork:
         self.n_images = len(self.image_links)
         self.n_words = len(self.all_words)
         self.n = self.n_images + self.n_words
-        self.g = [[] for i in range(self.n)]
+        self.g = [[] for i in range(self.n + 1)]
 
         # Image Links
         for i in range(self.n_images):
             for j in range(len(self.image_links[i])):
-                u = i
-                v = int(self.image_links[i][j][0] + self.n_images)
+                u = i + 1
+                v = self.image_links[i][j][0] + self.n_images + 1
                 if self.image_links[i][j][1] != 0:
                     w = 1.00 / self.image_links[i][j][1]
                     self.g[u].append((v, w))
@@ -45,8 +142,8 @@ class WordAssociationsNetwork:
         # Word Links
         for i in range(len(self.all_words)):
             for j in range(i, len(self.all_words)):
-                u = i + self.n_images
-                v = j + self.n_images
+                u = i + self.n_images + 1
+                v = j + self.n_images + 1
                 winv = max(self.word_links[i][j], self.word_links[j][i])
                 if winv != -1:
                     w = 1.00/winv
@@ -55,18 +152,21 @@ class WordAssociationsNetwork:
 
     def isEtherealNode(self, u):
         return u == -1
+    
+    def isTestNode(self, u):
+        return u == 0
 
     def isImageNode(self, u):
-        return u >= 0 and u < self.n_images; 
+        return u > 0 and u <= self.n_images; 
 
     def isWordNode(self, u):
-        return u >= self.n_images and u < self.n 
+        return u > self.n_images and u <= self.n 
     
     def dijkstra(self, sc, topK):
         vis  = [False for i in range(self.n + 1)]
         par = [-1 for i in range(self.n + 1)]
         dist = [1e9 for i in range(self.n + 1)]
-        imgdist = [1e9 for i in range(self.n_images)]
+        imgdist = [1e9 for i in range(self.n_images + 1)]
 
         dist[sc] = 0
         imgdist[sc] = 0
@@ -91,6 +191,93 @@ class WordAssociationsNetwork:
                 key = lambda index: imgdist[index]
         )[1:(topK + 1)]
         return ans
+    
+    def predict(self, img, topK):
+        image_link = get_predictions(resize_image_for_resnet(img))
 
-model = WordAssociationsNetwork()      
-print(model.dijkstra(218, 4))
+        base_new_words = set()
+        for i in range(topIWK):
+            if bisect.bisect_left(self.all_words, image_link[i][0]) == self.n_words:
+                base_new_words.add(image_link[i][0])
+        
+        if len(base_new_words) == 0:
+            print("All fine!\n")
+        else:
+            print("Oh boi!\n")
+            # all_new_words = get_new_words(base_new_words)
+            # for i in range(all_new_words):
+            #     id = len(self.all_words)
+            #     self.all_words.append(all_new_words[i])
+            #     self.id2word[id] = all_new_words[i]
+            #     self.word2id[all_new_words[i]] = id
+            # self.n_words = len(self.all_words)
+            # self.n = self.n_images + self.n_words
+
+        for i in range(len(image_link)):
+            u = 0
+            v = self.word2id[image_link[i][0]] + self.n_images + 1
+            w = 1.00 / float(image_link[i][1])
+            self.g[u].append((v, w))
+            self.g[v].append((u, w))
+
+        return model.dijkstra(0, topK)
+        
+
+train_data = CIFARData("train")
+test_data  = CIFARData("test")
+
+model = WordAssociationsNetwork()
+index = random.randrange(len(test_data.dataset))
+predictions = [x - 1 for x in model.predict(test_data.dataset[index]["img"], 4)]
+
+print("\nRandomly chosen test image index: {}\n".format(index))
+print("Predictions: {}".format(predictions))
+
+# create figure
+fig = plt.figure(figsize=(10, 10))
+  
+# setting values to rows and column variables
+rows = 3
+columns = 2
+  
+# Adds a subplot at the 1st position
+fig.add_subplot(rows, columns, 1)
+  
+# showing image
+plt.imshow(test_data.dataset[index]["img"])
+plt.axis('off')
+plt.title(test_data.id2fine_label[test_data.dataset[index]["fine_label"]])
+  
+# Adds a subplot at the 2nd position
+fig.add_subplot(rows, columns, 3)
+  
+# showing image
+plt.imshow(train_data.dataset[predictions[0]]["img"])
+plt.axis('off')
+plt.title(train_data.id2fine_label[train_data.dataset[predictions[0]]["fine_label"]])
+  
+# Adds a subplot at the 3rd position
+fig.add_subplot(rows, columns, 4)
+  
+# showing image
+plt.imshow(train_data.dataset[predictions[1]]["img"])
+plt.axis('off')
+plt.title(train_data.id2fine_label[train_data.dataset[predictions[1]]["fine_label"]])
+  
+# Adds a subplot at the 4th position
+fig.add_subplot(rows, columns, 5)
+  
+# showing image
+plt.imshow(train_data.dataset[predictions[2]]["img"])
+plt.axis('off')
+plt.title(train_data.id2fine_label[train_data.dataset[predictions[2]]["fine_label"]])
+
+# Adds a subplot at the 4th position
+fig.add_subplot(rows, columns, 6)
+  
+# showing image
+plt.imshow(train_data.dataset[predictions[3]]["img"])
+plt.axis('off')
+plt.title(train_data.id2fine_label[train_data.dataset[predictions[3]]["fine_label"]])
+
+plt.show()
